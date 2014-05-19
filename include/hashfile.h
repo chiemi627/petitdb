9131eb0c953408406f9efbcc8bc2dcb9ed5ce45c
Chiemi Watanabe <chiemi@acm.org>
2014-05-19 15:58:59 +0900

    Update hashfile.h

--- include/hashfile.h
+++ include/hashfile.h
@@ -10,19 +10,22 @@ typedef struct HFILE{
 } HFILE;
 
 HFILE *new_HFILE();
+void db_hashfile_create_and_open(HFILE *hfile,char *filename);
+void db_hashfile_prepare(HFILE *hfile,short nofbackets);
+int db_hashfile_open(HFILE *hfile,char *filename);
+int db_hashfile_close(HFILE *hfile);
+
 void db_hashfile_get_header(HFILE *hfile);
-void db_hashfile_get_page(HFILE *hfile,short pno);
 short db_hashfile_get_number_of_initial_backets(HFILE *hfile);
 short db_hashfile_get_number_of_all_backets(HFILE *hfile);
-void db_hashfile_prepare(HFILE *hfile,short nofbackets);
-short db_hashfile_get_the_last_page(HFILE *hfile);
+
 short db_hashfile_get_hash(char *value, short nofbackets);
-int db_hashfile_insert(HFILE *hfile, record *rd);
-record *db_hashfile_read(HFILE *hfile);
 void db_hashfile_get_page(HFILE *hfile,short pno);
-int db_hashfile_close(HFILE *hfile);
+record *db_hashfile_read(HFILE *hfile);
+
+short db_hashfile_get_the_last_page(HFILE *hfile);
+int db_hashfile_insert(HFILE *hfile, record *rd);
+void db_hashfile_write_page(HFILE *hfile);
+
 recordList *db_hashfile_search_by_scan(HFILE *hfile,char *keyword);
 recordList *db_hashfile_search_by_hash(HFILE *hfile,char *keyword);
-int db_hashfile_open(HFILE *hfile,char *filename);
-void db_hashfile_create_and_open(HFILE *hfile,char *filename);
-void db_hashfile_write_page(HFILE *hfile);
